62ffaa3c020b3220979ccb2eea6eebda3bbd2c09
Clarice Costello <claricostello@outlook.com>
2019-01-10 11:28:06 -0800

    wrote basic menu method

--- GuessingGameApp/GuessingGameApp/Program.cs
+++ GuessingGameApp/GuessingGameApp/Program.cs
@@ -8,11 +8,48 @@ namespace GuessingGameApp
         static void Main(string[] args)
         {
             string path = "../../../../../words.txt";
+
+            Menu();
+
             CreateFile(path);
             AppendToFile(path, "CAT");
             ReadFile(path);
+            //DeleteFile(path);
         }
 
+        static void Menu()
+        {
+            Console.WriteLine("Play HangMan?");
+            Console.WriteLine("1. Begin Game");
+            Console.WriteLine("2. Admin");
+            Console.WriteLine("3. Exit");
+            string pickStr = Console.ReadLine();
+
+            int pick = pickHandler(pickStr);
+
+            switch (pick)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        static int pickHandler(string pickStr)
+        {
+            int pick = Convert.ToInt32(pickStr);
+            return pick;
+        }
+
+
+
+        //System.IO functions
         static void CreateFile(string path)
         {
             using (StreamWriter streamWriter = new StreamWriter(path))
