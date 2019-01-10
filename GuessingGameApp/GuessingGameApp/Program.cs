3d9c104881ea6364473f2348a6845fbcb06c9014
Clarice Costello <claricostello@outlook.com>
2019-01-10 11:15:04 -0800

    scaffolded system.IO methods

---
+++ GuessingGameApp/GuessingGameApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GuessingGameApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void CreateFile(string path)
+        {
+
+        }
+        static void ReadFile(string path)
+        {
+
+        }
+        static void AppendToFile(string path)
+        {
+
+        }
+        static void DeleteFile(string path)
+        {
+
+        }
+    }
+}
